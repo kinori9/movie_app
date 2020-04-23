e9fbd35e3fe02b4cff7c15652f58301dbd0bd99c
kinori9 <50355001+kinori9@users.noreply.github.com>
2020-04-23 15:24:45 +0900

    Updates

---
+++ precache-manifest.e0e3119801a7fbf530e1f06e4ac1c1ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75f83aa76c1a7f618d5f431e56b19ca",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d13a56658e21d68e6c8e",
+    "url": "/movie_app/static/css/main.3a15d756.chunk.css"
+  },
+  {
+    "revision": "98be54eaa9ed6a5a4f95",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.cfee456d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13a56658e21d68e6c8e",
+    "url": "/movie_app/static/js/main.e78e66a1.chunk.js"
+  },
+  {
+    "revision": "4dcb005dec2f66fe5231",
+    "url": "/movie_app/static/js/runtime-main.615d8c0e.js"
+  }
+]);
